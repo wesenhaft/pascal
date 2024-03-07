4b07e102277963abb20fe9fdb47c334851756839
noreply@wesenhaft.com <noreply@wesenhaft.com>
2024-03-06 22:39:51 -0800

    -- version 2.0.001

---
+++ config.h
@@ -0,0 +1,7 @@
+/*
+Copyright (C) 2024 wesenhaft.com.  All rights reserved.
+For updates, please visit <https://www.wesenhaft.com/>.
+*/
+
+#define pascal_VERSION_MAJOR "2.0"
+#define pascal_VERSION_MINOR "001"
